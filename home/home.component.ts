d6bad54d59fd3311cc91ecb6078afac208945c9e
Bernat Ferragut <elbernat@gmail.com>
2017-07-20 16:18:12 -0400

    Add files via upload

---
+++ home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from './../animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [routerTransition],
+  host: {'[@routerTransition]': ''}
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
